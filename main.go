ed005943a23678ba7487e6df6ba8674b54a74fcb
moise-dev <moise.medici@gmail.com>
2025-09-15 21:37:51 +0100

    feat: commands exit and help

--- main.go
+++ main.go
@@ -3,25 +3,63 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func cleanInput(text string) []string {
 	return strings.Split(strings.TrimSpace(strings.ToLower(text)), " ")
 
 }
 
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
+
+}
 func main() {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "exit program",
+			callback:    commandExit,
+		},
 
-	scanner := bufio.NewScanner(strings.NewReader(""))
+		"help": {
+			name:        "help",
+			description: "usage help",
+			callback:    commandHelp,
+		},
+	}
 
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		for scanner.Scan() {
-			text := scanner.Text()
+		for !scanner.Scan() {
+			break
 		}
+		text := scanner.Text()
 		cleanText := cleanInput(text)
-		fmt.Println("Your command was: %s", cleanText[0])
+
+		command, ok := commands[cleanText[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
+
 	}
 
 }
